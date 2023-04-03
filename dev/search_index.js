57f694f1a90e8ac3c5a0334f3c0c5b5fae6be486
Documenter.jl <documenter@juliadocs.github.io>
2023-04-03 05:26:57 +0000

    build based on 52dcd6b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DynamicPlots.jl","page":"DynamicPlots.jl","title":"DynamicPlots.jl","text":"","category":"section"},{"location":"","page":"DynamicPlots.jl","title":"DynamicPlots.jl","text":"Documentation for DynamicPlots.jl","category":"page"}]
+}
