e67f4849b5728a4d2db26819e4a8b263919c1ca5
Niko Siccha <nikolassiccha@gmail.com>
2023-05-04 08:28:38 +0300

    added Histogram and exposed Plotter

--- src/DynamicPlots.jl
+++ src/DynamicPlots.jl
@@ -1,6 +1,7 @@
 module DynamicPlots
 
-export Plot, Figure, PlotSum, Line, Scatter, EmptyPlot#, PairPlot
+export Plot, Figure, Plotter, PlotSum, Line, Scatter, Histogram, EmptyPlot
+# export PairPlot
 using DynamicObjects
 using Plots
 import Markdown
@@ -17,16 +18,18 @@ Base.adjoint(what::Plot) = what
 dir(what::Plot) = "figs"
 path(what::Plot) = "$(what.dir)/$(hash(what)).png"
 alt_text(what::Plot) = ""
+plot_args(what::Plot) = Tuple([])
+plot_kwargs(what::Plot) = (label="",)
+
 function markdown(what::Plot)
     if !isfile(what.path)
-        if !isdir(what.dir)
-            mkpath(what.dir)
-        end
+        mkpath(what.dir)
         println("Saving $(what.path).")
         png(what.figure, what.path)
     end
     Markdown.parse("![$(what.alt_text)]($(what.path))")
 end
+Base.show(io::IO, mime::MIME"text/markdown", what::Plot) = show(io, mime, what.markdown)
 
 @dynamic_object Figure <: Plot plots::AbstractArray
 subplots(what::Figure) = figure.(what.plots')
@@ -44,38 +47,30 @@ initial_figure(what::Figure) = plot(what.subplots...; what.figure_kwargs..., wha
 Base.adjoint(what::Figure) = DynamicObjects.update(what, plots=what.plots')
 Base.:+(lhs::Figure, rhs::Figure) = DynamicObjects.update(lhs, plots=lhs.plots .+ rhs.plots)
 
-@dynamic_object Plotter <: Plot func::Function
-plot_args(what::Plot) = Tuple([])
-plot_kwargs(what::Plot) = NamedTuple()
-figure!(fig, what::Plotter) = (what.func(fig, what.plot_args...; label="", what.plot_kwargs...); fig) 
-
+@dynamic_object Plotter <: Plot func::Function plot_args
+figure!(fig, what::Plotter) = (what.func(fig, what.plot_args...; what.plot_kwargs...); fig) 
 
 @dynamic_object PlotSum <: Plot summands::AbstractArray
-# no_plots(what::PlotSum) = length(what.summands)
 function figure!(fig, what::PlotSum)
-    for summand in what.summands figure!(fig, summand; what.plot_kwargs...) end
+    for summand in what.summands figure!(fig, summand, what.plot_args...; what.plot_kwargs...) end
     fig
 end
 summands(what::Plot) = [what]
 Base.:+(lhs::Plot, rhs::Plot) = PlotSum([lhs.summands..., rhs.summands...])
 
 @dynamic_object Line <: Plot x y
-figure!(fig, what::Line) = (plot!(fig, what.x, what.y, label=""; what.plot_kwargs...); fig)
+figure!(fig, what::Line) = (plot!(fig, what.x, what.y, what.plot_args...; what.plot_kwargs...); fig)
 
 @dynamic_object Scatter <: Plot x y
-figure!(fig, what::Scatter) = (scatter!(fig, what.x, what.y, label=""; what.plot_kwargs...); fig) 
+figure!(fig, what::Scatter) = (scatter!(fig, what.x, what.y, what.plot_args...; what.plot_kwargs...); fig) 
+
+@dynamic_object Histogram <: Plot x y
+figure!(fig, what::Scatter) = (histogram!(fig, what.x, what.plot_args...; what.plot_kwargs...); fig) 
 
 
 @dynamic_object EmptyPlot <: Plot x y
 initial_figure(::EmptyPlot) = plot(xaxis=false, yaxis=false, xticks=false, yticks=false)
 figure!(fig, ::EmptyPlot) = fig
 
-
-
-
-# What is actually the best way to show plots?
-
-Base.show(io::IOContext{IOBuffer}, what::Plot) = display(what.markdown)
-# Base.display(how, what::Plot) = display(what.markdown)
 end
   
